b8bc509116b651216f4176a5b974ae82e0292b7a
RB-Codes <rasheedbader8@gmail.com>
2020-05-03 09:58:43 +0300

    added forms

--- company.js
+++ company.js
@@ -1,7 +1,22 @@
-function name(a) {
-     alert ('welcome')
 
-}
+function confirm() {
+     var txt;
+     if (confirm("Want to contiue?")) {
+       txt = "You are welcome!";
+     } else {
+       txt = "Why you Cancel?!";
+     }
 
-//we must give this company functions
-//also we need to give this company loops
+   }
+
+   function validateForm() {
+     var x = document.forms["myForm"]["fname"].value;
+     if (x == "") {
+       alert("Name must be filled out");
+       return false;
+     }
+   }
+
+   function welcome(
+        text = ("wlcome")
+   )
