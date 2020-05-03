038559f2ac8de47d1d8621b9d54f6edf4cb7d0d6
RB-Codes <rasheedbader8@gmail.com>
2020-05-03 13:41:43 +0300

    changed

--- company.js
+++ company.js
@@ -1,23 +1,54 @@
 
-function confirm() {
-     var txt;
-     if (confirm("Want to contiue?")) {
-       txt = "You are welcome!";
-     } else {
-       txt = "Why you Cancel?!";
-     }
+welcome();
+topage();
+//validateform();
 
-   }
+function welcome() {
+  alert("Welcome!")
 
-   function validateForm() {
-     var x = document.forms["myForm"]["fname"].value;
-     if (x == "") {
-       alert("Name must be filled out");
-       return false;
-     }
-   }
+}
 
-   function welcome() {
-          alert("Welcome")
+function topage() {
+  var txt;
+  var con2 = confirm("Want to contiue?");
+  if (con2) {
+    txt = "You are welcome!";
+    alert(txt);
+  } else {
+    txt = "Why you Cancel?!";
+    alert(txt);
+  }
 
-        }
+}
+
+//  function validateForm() {
+//  var x = document.forms["myForm"]["fname"].value;
+//  if (x == "") {
+//  alert("Name must be filled out");
+//  return false;
+//  }
+//  }
+
+function klik() {
+  var nomm = prompt("Give a number between 0 and 5")
+
+  while (nomm <= 0 || nomm > 5) {
+    nomm = prompt("Try again!")
+  }
+
+  for (var i =0; i < nomm; i++ ){
+    var stik = document.createElement('img');
+    stik.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/1/1a/USB_Drive.png');
+  
+   var container = document.getElementById('good');
+   container.appendChild(stik);  
+  }
+
+ 
+
+
+}
+
+
+
+
